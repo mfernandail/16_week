6f88301f80ea3e1e8bc4d816037888788d3f8e9e
mfernandail <maria.ibanez.leiva@gmail.com>
2025-08-08 21:38:07 -0400

    Fix typo in button variable name and streamline password generation logic

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-const btnGenetare = document.getElementById('generateBtn')
+const btnGenerate = document.getElementById('generateBtn')
 const numCharacterInput = document.getElementById('numCharacter')
 const includeNumbersCheck = document.getElementById('includeNumbers')
 const result = document.getElementById('result')
+const msgCopy = document.getElementById('msgCopy')
 
 const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 const LOWERCASE = UPPERCASE.map((letter) => letter.toLowerCase())
 const NUMBERS = '0123456789'.split('')
 const SYMBOLS = '!@#$%^&*()-_=+{}[]|:;<>,.?/'.split('')
 
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const numbers = '1234567890'
+let showMsg = false
 
-let includeNumbers
-
-let numCharacter = 52
-
-btnGenetare.addEventListener('click', generatePassword)
+btnGenerate.addEventListener('click', generatePassword)
+result.addEventListener('click', copyPassword)
 
 function generatePassword() {
   let pool = []
   let password = ''
 
-  if (
-    Number(numCharacterInput.value) < 4 ||
-    Number(numCharacterInput.value) >= 21
-  ) {
+  const lengthPassword = Number(numCharacterInput.value) || 0
+
+  if (lengthPassword < 4 || lengthPassword >= 21) {
     alert(
       'The password must be at least 4 characters and less than 21 characters long'
     )
@@ -43,4 +39,32 @@ function generatePassword() {
   }
 
   result.textContent = password
+}
+
+function copyPassword() {
+  if (!showMsg) {
+    const password = result.textContent.trim()
+    msgCopy.textContent = ''
+
+    if (password) {
+      navigator.clipboard
+        .writeText(password)
+        .then(() => {
+          msgCopy.textContent = 'Password copied to clipboard'
+          msgCopy.classList.add('copyPassword')
+        })
+        .catch((err) => {
+          msgCopy.textContent = `Error copying to clipboard ${err}`
+          msgCopy.classList.add('copyPassword copyPasswordError')
+        })
+    }
+    result.disabled = true
+    showMsg = true
+  }
+
+  setTimeout(() => {
+    msgCopy.textContent = ''
+    showMsg = false
+    result.disabled = false
+  }, 3000)
 }
